651614fbf8c53532189a19b9adca92a207a0f4f4
Gordan Sajević <gsajevic@hotmail.com>
2015-01-15 15:53:45 +0100

    added comments in LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -5,10 +5,19 @@ public class LinkedList<T> implements Iterable{
 	
 	private Node head;
 	
+	/**
+	 * Konstruktor bez parametara
+	 */
+	
 	public LinkedList()
 	{
 		this.head = head;
 	}
+	
+	/**
+	 * Metoda koja dodaje elemente listi
+	 * @param value
+	 */
 	
 	public void add(T value)
 	{
@@ -37,6 +46,11 @@ public class LinkedList<T> implements Iterable{
 		
 	}
 	
+	/**
+	 * Metoda koja uklanja element određen indeksom iz liste
+	 * @param index
+	 */
+	
 	public void removeAt(int index)
 	{
 		if (index < 0)
@@ -63,6 +77,11 @@ public class LinkedList<T> implements Iterable{
 		
 	}
 	
+	/**
+	 * Metoda koja vraća broj elemenata
+	 * @return counter
+	 */
+	
 	public int getSize()
 	{
 		Node current = head;
@@ -74,6 +93,12 @@ public class LinkedList<T> implements Iterable{
 		}
 		return counter;
 	}
+	
+	/**
+	 * Metoda koja vraća jedan element određen indeksom
+	 * @param index
+	 * @return
+	 */
 	
 	public T getAt(int index)
 	{
@@ -87,6 +112,10 @@ public class LinkedList<T> implements Iterable{
 		return (T) current.value;
 	}
 	
+	/**
+	 * Metoda koja vraća string sa svim elementima liste 
+	 */
+	
 	public String toString()
 	{
 		String str="";
@@ -99,16 +128,33 @@ public class LinkedList<T> implements Iterable{
 		return str;
 	}
 	
+	/**
+	 * node klasa
+	 * @author gordansajevic
+	 * @param <T>
+	 */
+	
 	private class Node<T>
 	{
 		private T value;
 		private Node next;
+		
+		/**
+		 * Konstrukor sa jednim parametrom
+		 * @param value
+		 */
 		
 		public Node(T value)
 		{
 			this.value = value;
 			this.next = null;
 		}
+		
+		/**
+		 * Konstruktor sa dva parametra
+		 * @param value
+		 * @param next
+		 */
 		
 		public Node (T value, Node next)
 		{
@@ -122,12 +168,27 @@ public class LinkedList<T> implements Iterable{
 		return new Biterator<T>(head);
 	}
 	
+	/**
+	 * Klasa koja implementira iterator
+	 * @author gordansajevic
+	 * @param <T>
+	 */
+	
 	private class Biterator<T> implements Iterator<T> {
 		private Node<T> current;
+		
+		/**
+		 * Konstruktor sa jednim parametrom
+		 * @param start
+		 */
 		
 		public Biterator(Node start) {
 			current = start;
 		}
+		
+		/**
+		 * Metoda koja provjerava da li postoji sljedeći element
+		 */
 
 		@Override
 		public boolean hasNext() {
